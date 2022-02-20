5703b10e2ee53c8503f4813977a07026261c5b8a
Murilo Oliveira de Araujo <muritavo@outlook.com>
2022-02-20 17:09:04 -0300

    Added a new typing to help omitting props from union

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,7 +1,20 @@
 export type CommonErrorCodes = "UNEXPECTED_ERROR";
 
-type Decompose<O extends object> = O
+type Decompose<O extends object> = O;
 
 export type BasicContext<T extends object> = {
   set: <P extends keyof T>(p: P, value: T[typeof p]) => void;
 } & T;
+
+/**
+ * This is usefull for omitting props from union
+ * Check whz contestation model to see an example
+ *
+ * If different models with different properties use the native Omit<O>
+ * Their differences are also omitted generating a single model
+ *
+ * This typing solves that
+ */
+export type DistributiveOmit<T, K extends keyof any> = T extends any
+  ? Omit<T, K>
+  : never;

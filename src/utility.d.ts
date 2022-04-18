1ff56befa826e9af537f2fb7619d405608ef652b
Murilo Oliveira de Araujo <muritavo@outlook.com>
2022-04-18 12:41:11 -0300

    Updated wallet types

--- src/utility.d.ts
+++ src/utility.d.ts
@@ -2,3 +2,8 @@ namespace OnepercentUtility {
   type IntlIds = "generic.id";
   type ApplicationABIs = any[];
 }
+declare module "use-wallet" {
+  import * as UseWallet from "use-wallet";
+  export const useWallet: typeof UseWallet['use-wallet'];
+  export const UseWalletProvider: typeof UseWallet['UseWalletProvider'];
+}

71605ebac8fd8add872a7c9d66b6d5c54e480db6
Murilo Oliveira de Araujo <muritavo@outlook.com>
2022-08-15 11:10:19 -0300

    Added a callback for when the user clicks out of the modal Fixed modal entrance on mobile

--- src/components/AdaptiveDialog/AdaptiveDialog.tsx
+++ src/components/AdaptiveDialog/AdaptiveDialog.tsx
@@ -2,6 +2,7 @@ import React, { PropsWithChildren, useEffect, useRef, useState } from "react";
 import Styles from "./AdaptiveDialog.module.scss";
 import MutableHamburgerButton from "../MutableHamburgerButton";
 import ScrollAndFocusLock from "../utilitary/ScrollAndFocusLock";
+import ReactDOM from "react-dom";
 
 /**
  * This component implements a generic drawer that displays it as a drawer on mobile and as a modal on desktop
@@ -10,11 +11,13 @@ export default function AdaptiveDialog({
   onClose,
   open = false,
   className = "",
+  onClickOut,
   children,
 }: PropsWithChildren<{
   className?: string;
   open: boolean;
   onClose?: () => void;
+  onClickOut?: () => void;
 }>) {
   const rootDivRef = useRef<HTMLDivElement>(null);
   const [isVisible, setIsVisible] = useState(open);
@@ -37,29 +40,38 @@ export default function AdaptiveDialog({
   }, [open]);
 
   return isVisible || open ? (
-    <div
-      ref={rootDivRef}
-      className={`${Styles.backdrop} ${open ? Styles.open : Styles.close} ${
-        expanded ? Styles.expanded : ""
-      }`}
-    >
-      <div className={`${Styles.container} ${className}`}>
-        <ScrollAndFocusLock open={open}>
-          {onClose && (
-            <MutableHamburgerButton
-              className={Styles.closeBtn}
-              onClick={onClose}
-              state="closed"
-              size={24}
-            />
-          )}
+    <>
+      {ReactDOM.createPortal(
+        <div
+          ref={rootDivRef}
+          className={`${Styles.backdrop} ${open ? Styles.open : Styles.close} ${
+            expanded ? Styles.expanded : ""
+          }`}
+          onClick={onClickOut}
+        >
           <div
-            className={Styles.indicator}
-            onClick={() => setExpanded((p) => !p)}
-          />
-          {children}
-        </ScrollAndFocusLock>
-      </div>
-    </div>
+            className={`${Styles.container} ${className}`}
+            onClick={(e) => e.stopPropagation()}
+          >
+            <ScrollAndFocusLock open={open}>
+              {onClose && (
+                <MutableHamburgerButton
+                  className={Styles.closeBtn}
+                  onClick={onClose}
+                  state="closed"
+                  size={24}
+                />
+              )}
+              <div
+                className={Styles.indicator}
+                onClick={() => setExpanded((p) => !p)}
+              />
+              {children}
+            </ScrollAndFocusLock>
+          </div>
+        </div>,
+        document.body
+      )}
+    </>
   ) : null;
 }

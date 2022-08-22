eca4ccb39ce8cf8c98c9666d69bdaacbab10e336
Murilo Oliveira de Araujo <muritavo@outlook.com>
2022-08-22 08:37:37 -0300

    Implemented new function for delegating pending async functions to a queue context

--- cypress/e2e/context/AsyncProcessQueue.test.tsx
+++ cypress/e2e/context/AsyncProcessQueue.test.tsx
@@ -1,40 +1,33 @@
 import { mount } from "cypress/react";
-import { CSSProperties, useState } from "react";
+import { CSSProperties, useLayoutEffect, useRef, useState } from "react";
+import AdaptiveDialog from "../../../src/components/AdaptiveDialog";
+import AnimatedEntrance from "../../../src/components/AnimatedEntrance/AnimatedEntrance";
+import Button from "../../../src/components/Button";
+import LoaderDotsIndicator from "../../../src/components/LoaderDotsIndicator";
 import Loader from "../../../src/components/LoaderDotsIndicator";
+import UncontrolledTransition from "../../../src/components/UncontrolledTransition";
 import {
   AsyncProcessQueueProvider,
   useAsyncProcessQueue,
   useAsyncProcessQueueContext,
 } from "../../../src/context/AsyncProcessQueue";
 import useAsyncControl from "../../../src/hooks/useAsyncControl";
+import TestStyles from "./AsyncProcessQueue.module.scss";
 
-it.only("Should animate the component from UI to queue Element", () => {
-  function DoSomeAsyncStuff({ exit }: { exit: () => void }) {
+it.each.only(
+  [
+    { width: 1920, height: 1800 },
+    // { width: 320, height: 480 },
+  ],
+  "Should animate the component from UI to queue Element",
+  (resolution) => {
+    let manualResolvers: (() => void)[] = [];
+    let manualRejectors: ((e: Error) => void)[] = [];
     const _doStuff = () =>
-      new Promise<void>((r) => {
-        setTimeout(() => {
-          r();
-        }, 2000);
+      new Promise<void>((r, rej) => {
+        manualResolvers.push(r);
+        manualRejectors.push(rej);
       });
-    const { doStuff: __doStuff, elToTransitionToQueue } = useAsyncProcessQueue({
-      doStuff: _doStuff,
-    });
-    const { loading, doStuff } = useAsyncControl({ doStuff: __doStuff });
-    return (
-      <h1
-        ref={elToTransitionToQueue}
-        style={{ position: "relative", top: "80vh", display: "inline-block" }}
-      >
-        This should animate to target{" "}
-        <button onClick={doStuff} disabled={loading}>
-          Click to start
-          {loading && <Loader dotsCount={10} />}
-        </button>
-        <button onClick={exit}>Click for magic</button>
-      </h1>
-    );
-  }
-  function Wrapper() {
     const style: CSSProperties = {
       display: "flex",
       alignItems: "center",
@@ -46,22 +39,31 @@ it.only("Should animate the component from UI to queue Element", () => {
       ...style,
       backgroundColor: "blue",
     };
-    const [elementExist, ss] = useState(true);
-    const target = useAsyncProcessQueueContext().targetElRef;
-    return (
-      <>
-        <div
-          style={{
-            width: 80,
-            height: 80,
-            ...style,
-            position: "absolute",
-            top: 32,
-            right: 32,
-          }}
-          ref={target as any}
-        >
-          <div style={{ width: 70, height: 70, ...style2 }}>
+    function Bullseye() {
+      const [o, so] = useState(false);
+      const { UIs, setUIs } = useAsyncProcessQueueContext();
+
+      return (
+        <>
+          <AdaptiveDialog
+            className={TestStyles.something}
+            open={o}
+            onClickOut={() => so(false)}
+          >
+            <AnimatedEntrance>{UIs}</AnimatedEntrance>
+            <Button
+              onClick={() =>
+                setUIs((prev) => prev.filter((ui) => ui.status !== "succeded"))
+              }
+            >
+              Clean
+            </Button>
+          </AdaptiveDialog>
+          <div
+            data-testid="bullseye"
+            onClick={() => so(true)}
+            style={{ width: 70, height: 70, ...style2 }}
+          >
             <div style={{ width: 60, height: 60, ...style }}>
               <div style={{ width: 50, height: 50, ...style2 }}>
                 <div style={{ width: 40, height: 40, ...style }}>
@@ -74,16 +76,153 @@ it.only("Should animate the component from UI to queue Element", () => {
               </div>
             </div>
           </div>
-        </div>
-        {elementExist && <DoSomeAsyncStuff exit={() => ss(false)} />}
-      </>
-    );
+        </>
+      );
+    }
+    function DoSomeAsyncStuff({ exit }: { exit: () => void }) {
+      const refNum = useRef(Math.random());
+      const {
+        doStuff: __doStuff,
+        elToTransitionToQueue,
+        wrapQueue,
+      } = useAsyncProcessQueue(
+        {
+          doStuff: _doStuff,
+        },
+        (funcName) => {
+          switch (funcName) {
+            case "doStuff":
+              return function Factory(state, error, dismiss, retry) {
+                switch (state) {
+                  case "loading":
+                    return (
+                      <UncontrolledTransition
+                        className={TestStyles.item}
+                        key={funcName + refNum.current}
+                      >
+                        <h1 key={state}>
+                          Cargando <LoaderDotsIndicator dotsCount={15} />
+                        </h1>
+                      </UncontrolledTransition>
+                    );
+                  case "succeded":
+                    return (
+                      <UncontrolledTransition
+                        className={TestStyles.item}
+                        key={funcName + refNum.current}
+                      >
+                        <h2 key={state}> Sucesso meu irmão</h2>
+                      </UncontrolledTransition>
+                    );
+                  case "failed":
+                    return (
+                      <UncontrolledTransition
+                        className={TestStyles.item}
+                        key={funcName + refNum.current}
+                      >
+                        <div key={state}>
+                          <h2>Falhou meu irmão {error.message}</h2>
+                          <Button onClick={dismiss}>Cancelar</Button>
+                          <Button onClick={retry}>Tentar denovo</Button>
+                        </div>
+                      </UncontrolledTransition>
+                    );
+                }
+              };
+          }
+        }
+      );
+      const { loading, doStuff } = useAsyncControl({ doStuff: __doStuff });
+
+      useLayoutEffect(() => wrapQueue, []);
+      return (
+        <h1
+          ref={elToTransitionToQueue}
+          style={{ position: "relative", top: "80vh", display: "inline-block" }}
+        >
+          This should animate to target{" "}
+          <button
+            onClick={() => {
+              doStuff().catch(() => {});
+            }}
+            disabled={loading}
+          >
+            Click to start
+            {loading && <Loader dotsCount={10} />}
+          </button>
+          <button onClick={exit}>Click for magic</button>
+        </h1>
+      );
+    }
+    function Wrapper() {
+      const [elementExist, ss] = useState(true);
+      const [elementExist2, ss2] = useState(true);
+      const [elementExist3, ss3] = useState(true);
+      const target = useAsyncProcessQueueContext().targetElRef;
+      return (
+        <>
+          <div
+            style={{
+              width: 80,
+              height: 80,
+              ...style,
+              position: "absolute",
+              top: 32,
+              right: 32,
+            }}
+            ref={target as any}
+          >
+            <Bullseye />
+          </div>
+          {elementExist && <DoSomeAsyncStuff exit={() => ss(false)} />}
+          {elementExist2 && <DoSomeAsyncStuff exit={() => ss2(false)} />}
+          {elementExist3 && <DoSomeAsyncStuff exit={() => ss3(false)} />}
+        </>
+      );
+    }
+    cy.on("uncaught:exception", () => false);
+    cy.viewport(resolution.width, resolution.height).then(() => {
+      mount(
+        <AsyncProcessQueueProvider>
+          <Wrapper />
+        </AsyncProcessQueueProvider>
+      ).wait(1000);
+      cy.get("button").eq(0).click().wait(100);
+      cy.get("button")
+        .eq(1)
+        .wait(100)
+        .click()
+        .wait(500)
+        .then(() => {
+          cy.get("button").eq(0).click().wait(100);
+          cy.get("button").eq(1).wait(100).click().wait(500);
+        })
+        .then(() => {
+          cy.get("button").eq(1).wait(100).click().wait(500);
+        });
+      cy.byTestId("bullseye").click();
+      cy.contains("Cargando .....").then(() => {
+        manualResolvers[0]();
+      });
+      cy.contains("Clean")
+        .click()
+        .wait(2000)
+        .then(() => {
+          manualRejectors[1](new Error("xpto"));
+        })
+        .wait(500)
+        .then(() => {
+          cy.contains("Tentar denovo")
+            .click()
+            .wait(500)
+            .then(() => {
+              manualResolvers[2]();
+            })
+            .wait(500)
+            .then(() => {
+              cy.contains("Clean").click();
+            });
+        });
+    });
   }
-  mount(
-    <AsyncProcessQueueProvider>
-      <Wrapper />
-    </AsyncProcessQueueProvider>
-  );
-  cy.get("button").first().click();
-  cy.get("button").last().wait(100).click();
-});
+);
--- src/context/AsyncProcessQueue.tsx
+++ src/context/AsyncProcessQueue.tsx
@@ -1,26 +1,55 @@
 import React, {
   createContext,
-  LegacyRef,
   PropsWithChildren,
+  ReactElement,
   RefObject,
+  useCallback,
   useContext,
-  useLayoutEffect,
+  useEffect,
   useRef,
+  useState,
 } from "react";
 
-const Context = createContext<{ targetElRef: RefObject<HTMLElement> }>(
-  null as any
-);
+type ReactElementWithState = ReactElement & {
+  status: "loading" | "succeded" | "failed";
+};
+
+const Context = createContext<{
+  targetElRef: RefObject<HTMLElement>;
+  pendingTransactions: ReturnType<typeof useCounter>;
+  UIs: ReactElementWithState[];
+  setUIs: (
+    func: (prev: ReactElementWithState[]) => ReactElementWithState[]
+  ) => void;
+}>(null as any);
+
+function useCounter() {
+  const [count, setCounter] = useState(0);
+
+  return {
+    count,
+    setCounter,
+    increment: () => setCounter((prev) => prev + 1),
+    decrement: () => setCounter((prev) => prev - 1),
+    reset: () => setCounter(0),
+  };
+}
 
 /**
  * This propagates the utilitary functions
  */
 export function AsyncProcessQueueProvider({ children }: PropsWithChildren<{}>) {
   const targetRef = useRef<HTMLElement>(null);
+  const pendingCounter = useCounter();
+  const [UIs, setUIs] = useState<ReactElementWithState[]>([]);
+
   return (
     <Context.Provider
       value={{
         targetElRef: targetRef,
+        pendingTransactions: pendingCounter,
+        setUIs,
+        UIs,
       }}
     >
       {children}
@@ -32,6 +61,25 @@ export function useAsyncProcessQueueContext() {
   return useContext(Context);
 }
 
+type AsyncProcessStatuses = "loading" | "succeded" | "failed";
+
+export interface UIStateFactory {
+  (
+    status: AsyncProcessStatuses,
+    error?: Error,
+    dismiss?: () => void,
+    retry?: () => void
+  ): ReactElement;
+}
+
+function calculateCenter(el: HTMLElement) {
+  const boundsOnViewport = el.getBoundingClientRect();
+  return {
+    x: boundsOnViewport.x + boundsOnViewport.width / 2,
+    y: boundsOnViewport.y + boundsOnViewport.height / 2,
+  };
+}
+
 /**
  * This function wraps other async functions and decides when the ongoing promise should be put on the queue or not
  */
@@ -39,80 +87,159 @@ export function useAsyncProcessQueue<
   T extends {
     [k: string]: (...args: any[]) => Promise<any>;
   }
->(functionsToQueue: T): T & { elToTransitionToQueue: RefObject<HTMLElement> } {
-  const { current: promisesToQueue } = useRef<Promise<any>[]>([]);
+>(
+  functionsToQueue: T,
+  UIFactory: (functionName: keyof T) => UIStateFactory
+): T & {
+  elToTransitionToQueue: RefObject<HTMLElement>;
+  /**
+   * Function that wraps the current running actions and animates to the target queue element
+   */
+  wrapQueue: () => void;
+} {
+  const loadingRef = useRef<Function | null>(null);
+  const wrapped = useRef(false);
   const elToTransitionToQueue = useRef<HTMLElement>(null);
-  const targetEl = useContext(Context).targetElRef;
-  useLayoutEffect(
-    () => () => {
-      const clone = elToTransitionToQueue.current!.cloneNode(
-        true
-      ) as HTMLElement;
-      const currPositionOnViewport =
-        elToTransitionToQueue.current!.getBoundingClientRect();
-
-      function calculateCenter(el: HTMLElement) {
-        const boundsOnViewport = el.getBoundingClientRect();
-        return {
-          x: boundsOnViewport.x + boundsOnViewport.width / 2,
-          y: boundsOnViewport.y + boundsOnViewport.height / 2,
-        };
-      }
-      const currPosCenter = calculateCenter(elToTransitionToQueue.current!);
-      const targetCenter = calculateCenter(targetEl.current!);
-      clone.style.position = "fixed";
-      clone.style.top = `${currPositionOnViewport.top}px`;
-      clone.style.left = `${currPositionOnViewport.left}px`;
-      clone.style.width = `${currPositionOnViewport.width}px`;
-      clone.style.height = `${currPositionOnViewport.height}px`;
-      clone.style.transition = `transform 500ms cubic-bezier(.3,.28,0,.95), opacity 500ms cubic-bezier(.25,.28,.86,-0.38)`;
-      clone.style.backgroundColor = "red";
-      clone.style.opacity = "1";
-
-      const targetHeight = Math.min(
-        targetEl.current!.clientHeight,
-        elToTransitionToQueue.current!.clientHeight
-      );
-      const targetWidth = Math.min(
-        targetHeight,
+  const { targetElRef: targetEl, setUIs } = useContext(Context);
+  const initialCenter =
+    useRef<{
+      center: ReturnType<typeof calculateCenter>;
+      dimensions: [number, number];
+    }>();
+  useEffect(() => {
+    initialCenter.current = {
+      center: calculateCenter(targetEl.current!),
+      dimensions: [
         targetEl.current!.clientWidth,
-        elToTransitionToQueue.current!.clientWidth
-      );
+        targetEl.current!.clientHeight,
+      ],
+    };
+  }, []);
+  const wrapQueue = useCallback(() => {
+    if (wrapped.current || !loadingRef.current) return;
+    const wrapUI = loadingRef.current;
+    wrapped.current = true;
 
-      const targetScaleX =
-        targetWidth / elToTransitionToQueue.current!.clientWidth;
+    const clone = elToTransitionToQueue.current!.cloneNode(true) as HTMLElement;
+    const currPositionOnViewport =
+      elToTransitionToQueue.current!.getBoundingClientRect();
+    const currPosCenter = calculateCenter(elToTransitionToQueue.current!);
+    const targetCenter = targetEl.current
+      ? calculateCenter(targetEl.current!)
+      : initialCenter.current!.center;
+    clone.style.position = "fixed";
+    clone.style.top = `${currPositionOnViewport.top}px`;
+    clone.style.left = `${currPositionOnViewport.left}px`;
+    clone.style.width = `${currPositionOnViewport.width}px`;
+    clone.style.height = `${currPositionOnViewport.height}px`;
+    clone.style.transition = `transform 250ms  ease-out, opacity 250ms ease-in`;
+    clone.style.opacity = "1";
+    clone.ontransitionend = ({ target, currentTarget }) => {
+      if (target === currentTarget) wrapUI();
+    };
 
-      const targetScaleY =
-        targetHeight / elToTransitionToQueue.current!.clientHeight;
+    const targetHeight = Math.min(
+      targetEl.current
+        ? targetEl.current!.clientHeight
+        : initialCenter.current!.dimensions[1],
+      elToTransitionToQueue.current!.clientHeight
+    );
+    const targetWidth = Math.min(
+      targetHeight,
+      targetEl.current
+        ? targetEl.current!.clientWidth
+        : initialCenter.current!.dimensions[0],
+      elToTransitionToQueue.current!.clientWidth
+    );
 
-      alert(`TX ${targetScaleX}`);
+    const targetScaleX =
+      targetWidth / elToTransitionToQueue.current!.clientWidth;
 
-      document.body.appendChild(clone);
-      setTimeout(() => {
-        clone.style.opacity = "0";
-        clone.style.transform = `translateX(${
-          targetCenter.x - currPosCenter.x
-        }px) translateY(${
-          targetCenter.y - currPosCenter.y
-        }px) scaleX(${targetScaleX}) scaleY(${targetScaleY})`;
+    const targetScaleY =
+      targetHeight / elToTransitionToQueue.current!.clientHeight;
+
+    document.body.appendChild(clone);
+    setTimeout(() => {
+      clone.style.opacity = "0";
+      clone.style.transform = `translateX(${
+        targetCenter.x - currPosCenter.x
+      }px) translateY(${
+        targetCenter.y - currPosCenter.y
+      }px) scaleX(${targetScaleX}) scaleY(${targetScaleY})`;
+      clone.addEventListener("transitionend", ({ target, currentTarget }) => {
+        if (target === currentTarget) clone.remove();
       });
-    },
-    []
-  );
+    }, 100);
+  }, []);
   return Object.entries(functionsToQueue).reduce(
-    (r, [k, v]) => ({
-      ...r,
-      [k]: (...args: any[]) => {
+    (r, [k, v]) => {
+      const process = (...args: any[]) => {
         const promise = v(...args);
-        promisesToQueue.push(promise);
+        loadingRef.current = function () {
+          // Write the loading UI
+          const LoadingUIInstance = {
+            ...UIFactory(k)("loading"),
+            status: "loading",
+          } as ReactElementWithState;
+          setUIs((prev) => [
+            ...prev.filter((a) => a.key !== LoadingUIInstance.key),
+            LoadingUIInstance,
+          ]);
+
+          promise.then((result) => {
+            const UIInstance = UIFactory(k)(
+              "succeded"
+            ) as ReactElementWithState;
+
+            // Write success UI
+            setUIs((prev) =>
+              prev.map((a) =>
+                a === LoadingUIInstance
+                  ? { ...UIInstance, status: "succeded" }
+                  : a
+              )
+            );
+
+            return result;
+          });
+          promise.catch((error) => {
+            const UIInstance = UIFactory(k)(
+              "failed",
+              error,
+              () => setUIs((prev) => prev.filter((ui) => ui !== UIInstance)),
+              () => {
+                process(...args);
+                loadingRef.current!();
+              }
+            ) as ReactElementWithState;
+            // Write success UI
+            setUIs((prev) =>
+              prev.map((a) =>
+                a === LoadingUIInstance
+                  ? { ...UIInstance, status: "failed" }
+                  : a
+              )
+            );
+
+            throw error;
+          });
+          return promise;
+        };
         promise.finally(() => {
-          promisesToQueue.splice(promisesToQueue.indexOf(promise), 1);
+          loadingRef.current = null;
         });
-        return promise;
-      },
-    }),
-    { elToTransitionToQueue } as T & {
+      };
+      return {
+        ...r,
+        [k]: process,
+      };
+    },
+    { elToTransitionToQueue, wrapQueue } as T & {
       elToTransitionToQueue: RefObject<HTMLElement>;
+      /**
+       * Function that wraps the current running actions and animates to the target queue element
+       */
+      wrapQueue: () => void;
     }
   );
 }

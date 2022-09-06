e6d012b9015fea2c2ac5ff48c3ecaac2116cd7df
Murilo Oliveira de Araujo <muritavo@outlook.com>
2022-09-06 16:45:52 -0300

    Abstracted behavior of freeze component to a hook

--- src/components/Freeze/Freeze.tsx
+++ src/components/Freeze/Freeze.tsx
@@ -1,10 +1,9 @@
 import React, { PropsWithChildren, useRef } from "react";
+import useFreeze from "../../hooks/useFreeze";
 
 /**
  * Use this component when you would like to freeze some child when this child can assume undefined in the next 'frame'
  **/
 export default function Freeze({ children }: PropsWithChildren<{}>) {
-  const currChildOrPrev = useRef<any>();
-  currChildOrPrev.current = children || currChildOrPrev.current;
-  return currChildOrPrev.current || null;
+  return useFreeze(children);
 }

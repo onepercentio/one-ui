43ba5e77ccc379b60b61a54144245dcac0451a91
Murilo Oliveira de Araujo <murilo.araujo@onepercent.io>
2022-10-03 14:55:08 -0300

    Making text/button variants customizable Changed orderable list style for better transition

--- src/utility.d.ts
+++ src/utility.d.ts
@@ -1,6 +1,6 @@
 /**
  * These are typings made to support separated function and prevent excessive Generics usage accross the application
- * 
+ *
  * Redeclare this namespace with the usefull typings for your project and enjoy intelisense :)
  */
 namespace OnepercentUtility {
@@ -12,17 +12,33 @@ namespace OnepercentUtility {
   namespace AsyncQueue {
     /** These are the recovery types available */
     type RecoveryTypes = {
-      [R: string]: any[]
-    }
+      [R: string]: any[];
+    };
 
     /** These are the available UI models */
     type UIModels = {
-      [k: string]: any[]
-    }
+      [k: string]: any[];
+    };
+  }
+  /** These are the extensible properties for the UI elements */
+  namespace UIElements {
+    type ButtonVariants = "transparent" | "filled" | "outline";
+    type TextVariants =
+      | "title"
+      | "description"
+      | "error"
+      | "caption"
+      | "highlightTitle"
+      | "highlight"
+      | "subtitle"
+      | "boldTitle"
+      | "link"
+      | "boldTitleBig"
+      | "content";
   }
 }
 declare module "use-wallet" {
   import * as UseWallet from "use-wallet";
-  export const useWallet: typeof UseWallet['use-wallet'];
-  export const UseWalletProvider: typeof UseWallet['UseWalletProvider'];
+  export const useWallet: typeof UseWallet["use-wallet"];
+  export const UseWalletProvider: typeof UseWallet["UseWalletProvider"];
 }

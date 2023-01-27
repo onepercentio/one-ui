82188d3e107f53fd9692b8b442b188ee6076d4e6
Murilo Oliveira de Araujo <murilo.araujo@onepercent.io>
2023-01-27 15:49:20 -0300

    Restricting logger functionality bound to global variable

--- src/models/DebugLogger.ts
+++ src/models/DebugLogger.ts
@@ -1,5 +1,7 @@
 const rand = require("color-seed");
+const win = window as any;
 
 export default function log(filter: string, obj: any) {
-  console.log(`%c ${filter}`, `color: ${rand.getColor(filter)}`, obj);
+  if (win.DEBUG === "*" || filter.includes(win.DEBUG))
+    console.log(`%c ${filter}`, `color: ${rand.getColor(filter)}`, obj);
 }

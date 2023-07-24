4e7bcb68a37d9513f5c477c6adcd620fe9e6889d
Murilo Oliveira de Araujo <murilo.araujo@onepercent.io>
2023-07-24 17:45:10 -0300

    Some fixes to storybook

--- src/storybookUtils/index.tsx
+++ src/storybookUtils/index.tsx
@@ -1,11 +1,31 @@
 import React, { ComponentProps, PropsWithChildren, forwardRef } from "react";
 import Spacing from "../components/Spacing";
+import { _Text } from "../components/Text/Text";
 
 export function extractAllPossibilitiesFromEnumProp<
   C extends (...args: any[]) => any,
   K extends keyof P,
   P extends ComponentProps<C> = ComponentProps<C>
 >(component: C, propName: K): P[K][] {
+  console.log(component, _Text, component === _Text);
+  if (component === _Text)
+    (_Text as any).__docgenInfo.props[propName] = {
+      type: {
+        value: [
+          "title",
+          "description",
+          "error",
+          "caption",
+          "highlightTitle",
+          "highlight",
+          "subtitle",
+          "boldTitle",
+          "link",
+          "boldTitleBig",
+          "content",
+        ].map((a) => ({ value: JSON.stringify(a) })),
+      },
+    };
   return (component as any).__docgenInfo.props[propName].type.value.map(
     (a: { value: string }) => JSON.parse(a.value)
   ) as P[K][];

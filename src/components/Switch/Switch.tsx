51d5068d320ec0bc5f77aa4ad3f248d032dc7027
Murilo Oliveira de Araujo <murilo.araujo@onepercent.io>
2023-08-04 15:08:07 -0300

    Allowing switch to be disabled

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -12,11 +12,13 @@ export default function Switch({
   enabled,
   onToggle,
   size,
+  disabled,
   ...props
 }: {
   enabled: boolean;
   onToggle: (newState: boolean) => void;
   size: number;
+  disabled?: boolean;
 } & Omit<
   DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>,
   "ref"
@@ -25,11 +27,14 @@ export default function Switch({
     <>
       <div
         style={{ fontSize: `${size}px` }}
-        className={`${Styles.root} ${enabled ? Styles.enabled : ""}`}
+        className={`${Styles.root} ${enabled ? Styles.on : ""} ${
+          disabled ? Styles.disabled : ""
+        }`}
         onClick={() => onToggle(!enabled)}
         {...props}
       >
         <div className={Styles.toggler} />
+        <input checked={enabled} type="checkbox" />
       </div>
     </>
   );

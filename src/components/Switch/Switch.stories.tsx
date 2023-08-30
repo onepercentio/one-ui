1f1144fc6d88f6e907a89830982a6e0c7843865a
Murilo Oliveira de Araujo <murilo.araujo@onepercent.io>
2023-08-30 15:34:16 -0300

    Improved switch story

--- src/components/Switch/Switch.stories.tsx
+++ src/components/Switch/Switch.stories.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from "react";
 import Switch from "./Switch";
-    
-export default {
-    component: Switch,
-    title: "Switch"
-}
 
-export const InitialImplementation = (args: any) => <Switch {...args}/>;
-InitialImplementation.args = {} as Partial<React.ComponentProps<typeof Switch>>
+export default {
+  component: Switch,
+  title: "Switch",
+};
+
+export const InitialImplementation = (args: any) => {
+  const [t, st] = useState(false);
+  return <Switch {...args} onToggle={st} enabled={t} />;
+};
+InitialImplementation.args = {} as Partial<React.ComponentProps<typeof Switch>>;

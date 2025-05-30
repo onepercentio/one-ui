d72ea175ecc09941cd8367db6d637533f6960316
Murilo Oliveira de Araujo <murilo.araujo@onepercent.io>
2025-05-30 00:41:25 -0300

    Added a new progress bar variant

--- cypress/e2e/components/ProgressBar/ProgressBar.stories.test.tsx
+++ cypress/e2e/components/ProgressBar/ProgressBar.stories.test.tsx
@@ -1,12 +1,35 @@
 import React from "react";
 import { mount } from "cypress/react";
 import * as AllExamples from "../../../../src/components/ProgressBar/ProgressBar.stories";
+import ProgressBar from "components/ProgressBar";
 
 it("All examples mount at least", () => {
-    for (let ExampleName in AllExamples) {
-        if (ExampleName === 'default') return;
-        const Example = AllExamples[ExampleName];
-        mount(<Example {...Example.args}/>)
-        cy.wait(500);
-    }
-})
+  for (let ExampleName in AllExamples) {
+    if (ExampleName === "default") return;
+    const Example = AllExamples[ExampleName];
+    mount(<Example {...Example.args} />);
+    cy.wait(500);
+  }
+});
+
+it.only("Should be able to show as various sections", () => {
+  cy.mount(
+    <div
+      style={{
+        "--progress-bar-bg": "red",
+        "--progress-bar-guide": "blue",
+        "--progress-bar-bar": "green",
+      }}
+    >
+      <br />
+      <h1>GAUGE</h1>
+      <ProgressBar progress={50} size={40} mode="gauge" />
+      <br />
+      <h1>GUIDE</h1>
+      <ProgressBar progress={50} size={40} mode="guide" />
+      <br />
+      <h1>sections</h1>
+      <ProgressBar progress={21} size={10} mode="sections" sections={10} />
+    </div>
+  );
+});
--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -1,25 +1,92 @@
-import React, { ComponentProps, useMemo } from "react";
+import React, {
+  ComponentProps,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import Styles from "./ProgressBar.module.scss";
 
 /**
  * Shows a progress bar
  **/
 export default function ProgressBar({
-  size,
   progress,
-  mode = "guide",
+  ...props
 }: {
-  /**
-   * Given as css font size
-   */
-  size: number | string;
   /**
    * Given in percent
    */
   progress: number;
-
-  mode?: "gauge" | "guide";
-}) {
+} & (
+  | {
+      /**
+       * Given as css font size
+       */
+      size: number | string;
+      mode?: "gauge" | "guide";
+    }
+  | {
+      /**
+       * Given as pixels
+       */
+      size: number;
+      mode?: "sections";
+      /** The number of sections to split */
+      sections: number;
+    }
+)) {
+  if (props.mode === "sections") {
+    const ref = useRef<SVGSVGElement>(null);
+    const strokeID = useMemo(
+      () => `stroke-${(Math.random() * 100000).toFixed(0)}`,
+      []
+    );
+    const height = props.size;
+    const [width, setWidth] = useState(0);
+    const dashWidth = width / props.sections;
+    const spacing = dashWidth * 0.2;
+    useEffect(() => {
+      setWidth(ref.current!.clientWidth);
+    }, []);
+    return (
+      <svg
+        ref={ref}
+        viewBox={`${-height / 2} 0 ${width + height - spacing} ${height}`}
+        preserveAspectRatio="none"
+        width={"100%"}
+        height={`${height}px`}
+      >
+        <defs>
+          <linearGradient
+            id={`${strokeID}`}
+            x1="0%"
+            y1="0%"
+            x2="100%"
+            y2="0%"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop offset="0%" stop-color="blue" />
+            <stop offset={`${progress}%`} stop-color="blue" />
+            <stop offset={`${progress}%`} stop-color="green" />
+            <stop offset="100%" stop-color="green" />
+          </linearGradient>
+        </defs>
+        <line
+          x1="0"
+          y1={height / 2}
+          x2={width}
+          y2={height / 2}
+          stroke={`url(#${strokeID})`}
+          stroke-width={height}
+          stroke-dasharray={`${dashWidth - spacing},${spacing}`}
+          stroke-linecap="round"
+        />
+      </svg>
+    );
+  }
+  const mode = props.mode || "guide";
+  const size = props.size;
   return (
     <div
       className={`${Styles.container} ${Styles[mode]} ${

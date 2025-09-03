7b923586a0eb3ba8bb8a0c0355b502f0bd00e40f
Murilo Oliveira de Araujo <murilo.araujo@onepercent.io>
2025-09-03 16:21:40 -0300

    Restricting useFreeze behaviour to only undefined value, as cenarios null = no value, 0 = real value, or "" = No text defined, could be confused with old value recovery

--- src/hooks/useFreeze.ts
+++ src/hooks/useFreeze.ts
@@ -1,11 +1,12 @@
 import { useRef } from "react";
 
 /**
- * This hook pipes a data and when it is set to a falsy value ("", undefined, null, 0), 
+ * This hook pipes a data and when it is set to a falsy value ("", undefined, null, 0),
  * it returns the previous valid value
  */
 export default function useFreeze<T>(something: T) {
-    const currChildOrPrev = useRef<T>(undefined);
-    currChildOrPrev.current = something || currChildOrPrev.current;
-    return currChildOrPrev.current || null;
+  const currChildOrPrev = useRef<T>(undefined);
+  currChildOrPrev.current =
+    something === undefined ? currChildOrPrev.current : something;
+  return currChildOrPrev.current || something;
 }
